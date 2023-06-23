9e2a617b3a22aa60b95b2b3a5fd5da499abe8742
jesse <jesse1819@gmail.com>
2023-06-23 17:01:13 -0400

    add server

--- openai-chat-yaml.groovy
+++ openai-chat-yaml.groovy
@@ -56,7 +56,7 @@ class OpenAIChat {
                 } else if (consoleInput.contains("4c")) {
                     systemRoleInitContent = 'Act as a master programmer, respond in code for specified language only.'
                 } else {
-                    systemRoleInitContent = "Answer concisely, precisely, no summaries. Say '' or 'sry' for apologies and proceed."
+                    systemRoleInitContent = "Answer concisely, precisely, no summaries. Say 's' or 'sry' for apologies and proceed."
                 }
                 int modelChoice = arrayIndexSelected != 4 ? 1 : 0
                 int max_tokenChoice = arrayIndexSelected != 4 ? 3200 : 7200
@@ -89,7 +89,7 @@ class OpenAIChat {
         def aiResponse
         def serverResponse
         try {
-            serverResponse = sendRequest('POST', url, jsonString, false, true).result
+            serverResponse = invokeSendRequest('POST', url, jsonString, false, true).result
             aiResponse = serverResponse.choices[0].message.content
         } catch (Exception e) {
             println "Error occurred while sending the request: ${e.message}"
@@ -146,43 +146,24 @@ class OpenAIChat {
         }
     }
 
-    def sendRequest(String reqMethod, String URL, String message, boolean failOnError, boolean useProxy){
-        Authenticator authenticator = new Authenticator() {
-            public PasswordAuthentication getPasswordAuthentication() {
-                return (new PasswordAuthentication(username,
-                        password.toCharArray()))
-            }
-        }
-        def response = [:]
-        def request
-        if(false){
-            Authenticator.setDefault(authenticator)
-            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.com", 8080))
-            request = new URL(URL).openConnection(proxy)
-        } else {
-            request = new URL(URL).openConnection()
-        }
-        request.setDoOutput(true)
-        request.setRequestMethod(reqMethod)
-        request.setRequestProperty('Authorization', "Bearer $token")
-        request.setRequestProperty('Content-Type', 'application/json')
-        if(!message.isEmpty())
-            request.getOutputStream().write(message.getBytes("UTF-8"))
-        def getRC = request.getResponseCode()
-        response.rc = getRC
-        def slurper = new JsonSlurper()
-        def result
-        try {
-            if(request.getInputStream().available())
-                result = slurper.parseText(request.getInputStream().getText())
-            response.result = result
-        } catch (Exception ignored) {
-            if(failOnError){
-                throw new Exception("Request made to $URL failed.\nResponse code is: $getRC\n${request.getResponseMessage()}\n${request.getErrorStream().getText()}")
-            } else{
-                response.result = request.getErrorStream().getText()
-            }
-        }
+    def invokeSendRequest(String reqMethod, String URL, String message, boolean failOnError, boolean useProxy) {
+        def serverUrl = 'http://localhost:9001/sendRequest'
+        def connection = new URL(serverUrl).openConnection()
+        connection.setRequestMethod('POST')
+        connection.setDoOutput(true)
+        connection.setRequestProperty('Content-Type', 'application/json')
+
+        def payload = [
+            reqMethod: reqMethod,
+            URL: URL,
+            message: message,
+            failOnError: failOnError,
+            useProxy: useProxy
+        ]
+
+        connection.getOutputStream().write(new JsonBuilder(payload).toString().getBytes("UTF-8"))
+
+        def response = new JsonSlurper().parseText(connection.getInputStream().getText())
         return response
     }
 }
